0a5006126aa8db366ddc6765e1523411c018a4be
Bipul Chaudhary <bipulchau@gmail.com>
2022-09-25 12:13:52 -0400

    Iterating array using for loop

--- src/CoreJavaBrushUp1.java
+++ src/CoreJavaBrushUp1.java
@@ -23,6 +23,13 @@ public class CoreJavaBrushUp1 {
 		
 		System.out.println(arr2[3]);
 		
+		// for loop
+		System.out.println("Looping through array:");
+		for(int i = 0; i < arr.length; i++) {
+			
+			System.out.println(arr[i]);
+		}
+		
 	}
 
 }
