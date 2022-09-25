16609f094a6281d15520fafb385e60aa33ebc68d
Bipul Chaudhary <bipulchau@gmail.com>
2022-09-24 23:10:14 -0400

    introduction to Java variables and Data types

---
+++ src/CoreJavaBrushUp1.java
@@ -0,0 +1,16 @@
+
+public class CoreJavaBrushUp1 {
+
+	public static void main(String[] args) {
+		int myNum = 5;
+		String name = "Bipul Chaudhary";
+		char ch = 'O';
+		double dec = 5.99;
+		boolean bool = true;
+		
+		System.out.println(myNum + " is the value stored in the myNum variable");
+		System.out.println("My name is " + name +".");
+		
+	}
+
+}
