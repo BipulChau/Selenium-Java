fee13bedcea1046f2aac2e972758572cfc2d5f19
Bipul Chaudhary <bipulchau@gmail.com>
2022-09-25 23:28:46 -0400

    static method

--- src/MethodsDemo.java
+++ src/MethodsDemo.java
@@ -6,12 +6,22 @@ public class MethodsDemo {
 		MethodsDemo d = new MethodsDemo();
 		String name = d.getData();
 		System.out.println(name);
+		
+		MethodsDemo2 d1 = new MethodsDemo2();
+		System.out.println(d1.getUserData());
+		
+		String d2 = getName();
+		System.out.println(d2);
 
 	}
 	
 	public String getData() {
 		System.out.println("Hello World");
 		return "Oggy Chaudhary";
+	}
+	
+	public static String getName() {
+		return "I am Static Method";
 	}
 
 }
---
+++ src/MethodsDemo2.java
@@ -0,0 +1,10 @@
+
+public class MethodsDemo2 {
+
+	public String getUserData() {
+		System.out.println("This is method from MethodsDemo2 Class");
+		return "Bunu Chaudhary";
+	}
+
+
+}
