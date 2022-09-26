1ec76f194c68533c8dce3dbc40de38d3c1723a78
Bipul Chaudhary <bipulchau@gmail.com>
2022-09-25 22:40:14 -0400

    String split and trim

---
+++ src/CoreJavaBrushUp3.java
@@ -0,0 +1,14 @@
+
+public class CoreJavaBrushUp3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Bipul Chaudhary Bunu Nanu Mum Oggy Madhu";
+		String[] s2 = s1.split(" ");
+		for (String s: s2) {
+			System.out.println(s.trim());
+		}
+
+	}
+
+}
