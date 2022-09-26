5cbce525e7f9e50028c55dee69da1c7f3f0dd241
Bipul Chaudhary <bipulchau@gmail.com>
2022-09-26 12:01:31 -0400

    Locators - id, name, className

---
+++ src/Locators.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+		driver.findElement(By.className("signInBtn")).click();
+		
+//		WebElement input = driver.findElement(By.xpath("//input[@name='username']"));
+//		input.click();
+//		input.sendKeys("Admin");
+	}
+
+}
