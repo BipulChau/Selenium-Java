2edc941375b958be8855c2b8624926c45cc98e3d
Bipul Chaudhary <bipulchau@gmail.com>
2022-09-26 21:27:41 -0400

    implict wait

--- src/Locators.java
+++ src/Locators.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,14 +13,16 @@ public class Locators {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
 		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
 		driver.findElement(By.className("signInBtn")).click();
 		
-//		WebElement input = driver.findElement(By.xpath("//input[@name='username']"));
-//		input.click();
-//		input.sendKeys("Admin");
+		//css selector
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+		
+
 	}
 
 }
