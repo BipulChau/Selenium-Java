41f0cb3d5167749727a35b1688ed2821aa78e2c2
Bipul Chaudhary <bipulchau@gmail.com>
2022-10-18 14:30:55 -0400

    static method that gets password

--- src/Locators2.java
+++ src/Locators2.java
@@ -15,9 +15,10 @@ public class Locators2 {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String password = getPassword(driver);
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys(name);
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		driver.findElement(By.className("signInBtn")).click();
 		
 		Thread.sleep(2000);
@@ -31,6 +32,17 @@ public class Locators2 {
 		driver.findElement(By.xpath("//button[text()='Log Out']")).click();
 		driver.close();
 
+	}
+	
+	public static String getPassword(WebDriver driver) throws InterruptedException {
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+		String[] passwordArray = passwordText.split("'");
+		String password = passwordArray[1].split("'")[0];
+		return password;
 	}
 
 }
